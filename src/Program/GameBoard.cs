bff4d8b7803a6080b67bccd507fd6dfe241a9767
Trinidad Bunge <trinibunge@gmail.com>
2025-09-03 11:59:25 -0300

    Cambios

--- src/Program/GameBoard.cs
+++ src/Program/GameBoard.cs
@@ -1,11 +1,10 @@
 namespace Ucu.Poo.GameOfLife;
 public class GameBoard
 {
-    private bool[,] gameBoard;
+    private bool[,] gameBoard = board.txt;
 
-    // Propiedades para obtener ancho y alto
-    public int boardWidth => gameBoard.GetLength(0);
-    public int boardHeight => gameBoard.GetLength(1);
+    public int boardWidth = gameBoard.GetLength(0);
+    public int boardHeight = gameBoard.GetLength(1);
 
     public GameBoard(int width, int height)
     {
@@ -33,5 +32,45 @@ public class GameBoard
             }
         }
         return cloneboard;
+    }
+    
+    public void UpdateBoard()
+    {
+        bool[,] cloneboard = new bool[boardWidth, boardHeight];
+
+        for (int x = 0; x < boardWidth; x++)
+        {
+            for (int y = 0; y < boardHeight; y++)
+            {
+                int aliveNeighbors = 0;
+
+                for (int i = x - 1; i <= x + 1; i++)
+                {
+                    for (int j = y - 1; j <= y + 1; j++)
+                    {
+                        if (i >= 0 && i < boardWidth &&
+                            j >= 0 && j < boardHeight &&
+                            gameBoard[i, j])
+                        {
+                            aliveNeighbors++;
+                        }
+                    }
+                }
+
+                if (gameBoard[x, y])
+                    aliveNeighbors--; 
+
+                if (gameBoard[x, y] && aliveNeighbors < 2)
+                    cloneboard[x, y] = false; 
+                else if (gameBoard[x, y] && aliveNeighbors > 3)
+                    cloneboard[x, y] = false; 
+                else if (!gameBoard[x, y] && aliveNeighbors == 3)
+                    cloneboard[x, y] = true; 
+                else
+                    cloneboard[x, y] = gameBoard[x, y]; 
+            }
+        }
+
+        gameBoard = cloneboard;
     }
 }
