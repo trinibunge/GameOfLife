a1e574506fb6562c8629ae10940bb4c17064963a
Trinidad Bunge <trinibunge@gmail.com>
2025-09-03 11:41:53 -0300

    Cambios

--- src/Program/GameBoard.cs
+++ src/Program/GameBoard.cs
@@ -1,52 +1,37 @@
 namespace Ucu.Poo.GameOfLife;
-
 public class GameBoard
 {
-    bool[,] gameBoard = /;
-    int boardWidth = gameBoard.GetLength(0);
-    int boardHeight = gameBoard.GetLength(1);
+    private bool[,] gameBoard;
 
-    bool[,] cloneboard = new bool[boardWidth, boardHeight];
-        for (int x = 0; x < boardWidth; x++)
+    // Propiedades para obtener ancho y alto
+    public int boardWidth => gameBoard.GetLength(0);
+    public int boardHeight => gameBoard.GetLength(1);
+
+    public GameBoard(int width, int height)
     {
-        for (int y = 0; y < boardHeight; y++)
+        gameBoard = new bool[width, height];
+    }
+
+    public bool GetCell(int x, int y)
+    {
+        return gameBoard[x, y];
+    }
+
+    public void SetCell(int x, int y, bool value)
+    {
+        gameBoard[x, y] = value;
+    }
+
+    public GameBoard Clone()
+    {
+        GameBoard cloneboard = new GameBoard(boardWidth, boardHeight);
+        for (int x = 0; x < boardWidth; x++)
         {
-            int aliveNeighbors = 0;
-            for (int i = x-1; i<=x+1;i++)
+            for (int y = 0; y < boardHeight; y++)
             {
-                for (int j = y-1;j<=y+1;j++)
-                {
-                    if(i>=0 && i<boardWidth && j>=0 && j < boardHeight && gameBoard[i,j])
-                    {
-                        aliveNeighbors++;
-                    }
-                }
-            }
-            if(gameBoard[x,y])
-            {
-                aliveNeighbors--;
-            }
-            if (gameBoard[x,y] && aliveNeighbors < 2)
-            {
-                //Celula muere por baja población
-                cloneboard[x,y] = false;
-            }
-            else if (gameBoard[x,y] && aliveNeighbors > 3)
-            {
-                //Celula muere por sobrepoblación
-                cloneboard[x,y] = false;
-            }
-            else if (!gameBoard[x,y] && aliveNeighbors == 3)
-            {
-                //Celula nace por reproducción
-                cloneboard[x,y] = true;
-            }
-            else
-            {
-                //Celula mantiene el estado que tenía
-                cloneboard[x,y] = gameBoard[x,y];
+                cloneboard.SetCell(x, y, GetCell(x, y));
             }
         }
+        return cloneboard;
     }
-    gameBoard = cloneboard;
 }
